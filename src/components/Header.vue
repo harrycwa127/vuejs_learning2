c5b982acfc53808f46cc5e2bf265c3ee24109998
Harry Tsang <harrytsang@fdssolutions.com.hk>
2025-11-17 16:10:42 +0800

    update UI and add data.json

---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <header class="header">
+    <h1 class="logo">
+      推甄分享網站
+    </h1>
+
+    <button class="add-btn">
+      + 新增文章
+    </button>
+  </header>
+</template>
---
+++ src/components/PostTable.vue
@@ -0,0 +1,34 @@
+<template>
+    <table class="post-table">
+        <thead>
+            <tr>
+                <th>學校</th>
+                <th>系所</th>
+                <th>年度</th>
+                <th>日期</th>
+                <th>Score</th>
+                <th>結果</th>
+                <th>來源</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="post in posts" :key="post.pId">
+                <td>{{ post.pSchool }}</td>
+                <td>{{ post.pDep }}</td>
+                <td>{{ post.pYear }}</td>
+                <td>{{ post.pDate }}</td>
+                <td>{{ post.pScore ?? '-' }}</td>
+                <td>{{ post.pResult1 }}</td>
+                <td>
+                    <a v-if="post.pURL" :href="post.pURL" target="_blank">來源</a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script setup>
+    const props = defineProps({
+        posts: { type: Array, default: () => [] }
+    })
+</script>
