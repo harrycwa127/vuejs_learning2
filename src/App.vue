544fe3064b8ddb6e8e43f53b579e542e3ac18705
Harry Tsang <harrytsang@fdssolutions.com.hk>
2025-11-17 16:23:51 +0800

    update table display

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,28 @@
 import Header from './components/Header.vue'
 import posts from './data/data.json'
 import PostTable from './components/PostTable.vue'
+import { ref, computed } from 'vue'
 
+const keyword = ref('')
+
+const filteredPosts = computed(() => {
+  if (!keyword.value) return posts
+  return posts.filter((p) => {
+    return (
+      p.pSchool?.includes(keyword.value) ||
+      p.pDep?.includes(keyword.value) ||
+      p.pResult1?.includes(keyword.value)
+    )
+  })
+});
 </script>
 
 <template>
   <Header />
   <main>
     <h2>經驗分享</h2>
-    <PostTable :posts="posts" />
+    <input v-model="keyword" type="search" placeholder="請輸入原學校、系所或欲推甄校系" class="search-box" />
+    <PostTable :posts="filteredPosts" v-if="filteredPosts.length > 0"/>
+    <p v-else>查無相關結果 QQ</p>
   </main>
 </template> 
